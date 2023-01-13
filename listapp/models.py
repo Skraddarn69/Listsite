ad1c7c2670c8c8c9a8323cbf4fcac25b98b44e36
LeifAG <leif.hammar@gymnasium.ax>
2023-01-13 10:41:00 +0200

    mall template

--- listapp/models.py
+++ listapp/models.py
@@ -1,7 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 class List(models.Model):
-    listname=models.charField(max_length=255)
+    listname=models.CharField(max_length=255)
     cuser=models.ForeignKey(User,on_delete=models.CASCADE)
     cdate=models.DateTimeField(default=timezone.now)
 
+    def __str__(self):
+        return self.listname
+
+class Object(models.Model):
+    objectname=models.CharField(max_length=255)
+    amount=models.DecimalField(max_digits=5,decimal_places=2)
+    purchased=models.BooleanField(default=False)
+    list=models.ForeignKey(List,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.objectname
